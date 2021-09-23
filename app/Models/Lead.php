6f8eba6dbdfbc021839e5d9e1264835f33fa850a
gagan ahuja <gdsa006@gmail.com>
2021-09-23 06:12:46 +0530

    csrf fix

--- app/Models/Lead.php
+++ app/Models/Lead.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class Location extends Authenticatable
+class Lead extends Authenticatable
 {
     protected $table = 'leads';
 }
