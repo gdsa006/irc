2ad0cc94a88fe7e3178012fcf00b36d02048402c
gagan ahuja <gdsa006@gmail.com>
2021-09-23 05:42:37 +0530

    csrf fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}" />
 
         <title>Laravel1</title>
 
@@ -1228,6 +1229,14 @@ input[type="text"], textarea {
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
+    <script type="text/javascript">
+$.ajaxSetup({
+    headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+});
+</script>
+
     <script>
             $('#startHere').click(function(){
                 $('#address').focus();
@@ -1249,7 +1258,7 @@ input[type="text"], textarea {
         $.ajax({
             type : 'get',
             url : '{{URL::to('search')}}',
-            data:{'_token': '{{ csrf_token() }}', 'search':$value},
+            data:{'search':$value},
             success:function(data){
                 console.log(data.search);
                 var ul = $('<ul>').appendTo('#search-results');
@@ -1281,12 +1290,10 @@ input[type="text"], textarea {
         $value=$("#address").val();
         console.log($value);
         $('#hidden-address').val('000');
-        "_token": "{{ csrf_token() }}",
-
         $.ajax({
             type : 'get',
             url : '{{ route('steps') }}',
-            data:{'_token': '{{ csrf_token() }}', 'address':$value},
+            data:{'address':$value},
             success:function(data){
                 console.log(data);
                 if(data.status != "not found"){
@@ -1298,13 +1305,7 @@ input[type="text"], textarea {
         });
     })
 </script>
-<!-- <script>
-$.ajaxSetup({
-  headers: {
-    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-  }
-});
-</script> -->
+
 </body>
     </body>
 </html>
