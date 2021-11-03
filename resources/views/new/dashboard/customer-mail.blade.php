6748304953767b018f75717369134ad779331ba0
gdsa006 <gdsa006@gmail.com>
2021-11-03 16:13:01 +0530

    chaanges

--- resources/views/new/dashboard/customer-mail.blade.php
+++ resources/views/new/dashboard/customer-mail.blade.php
@@ -8,7 +8,7 @@
 body {
 margin:0;
 padding:0;
-background-color: red;
+background-color: #ddd;
 text-align: center;
 	}
 
@@ -18,7 +18,7 @@ text-align: center;
 	}
 
 .back-div {
-background-color: #333;
+background-color: #269bd6;
 padding: 1.5em;
 margin: 3em 0 3em 5em;
 	}
@@ -39,7 +39,13 @@ padding: 1.5em;
     }
 
     #btn{
-        background-color: #269bd6
+        background-color: #269bd6;
+    color: #fff;
+    padding: 16px 50px;
+    text-decoration: none;
+    border-radius: 4px;
+    margin: 12px 0;
+    display: inline-block;
     }
 
 </style>
@@ -49,30 +55,104 @@ padding: 1.5em;
 
 
 
-    <div id="container">
-  <div class="back-div">
-	<div class="front-div">
 
-    <p>Hi {{ $details2['fname'] }} {{ $details2['lname'] }},</p><br>
+
+
+
+
+
+
+
+
+
+
+
+<table width="100%" border="0" align="center" cellpadding="0" cellspacing="0">
+
+    <!-- START HEADER/BANNER -->
+    
+            <tbody><tr>
+                <td align="center">
+                    <table class="col-600" width="600" border="0" align="center" cellpadding="0" cellspacing="0">
+                        <tbody><tr>
+                            <td align="center" valign="top" background="#ffffff" bgcolor="#ffffff" style="background-size:cover; background-position:top;height=" 400""="">
+                                <table class="col-600" width="600" height="400" border="0" align="center" cellpadding="0" cellspacing="0">
+    
+                                    <tbody><tr>
+                                        <td height="40"></td>
+                                    </tr>
+    
+    
+                                    <tr>
+                                        <td align="center" style="line-height: 0px;">
+                                            <img style="display:block; line-height:0px; font-size:0px; border:0px; margin-bottom: 20px" src="{{env('APP_NAME')}}/images/Final%20Logo-01.png" height="65" alt="logo">
+                                        </td>
+                                    </tr>
+    
+    
+    
+    
+    
+    
+                                    <tr>
+                                        <td align="left" style="font-family: 'Lato', sans-serif; padding: 5px 30px; font-size:15px; color:#000000; line-height:24px; font-weight: 300;">
+                                            
+                                            <p>Hi <b>{{ $details2['fname'] }} {{ $details2['lname'] }}</b>,</p>
  
-<p>It's Jake from Idahoroofingcost.com.</p>
+                                            <p>It's Jake from Idahoroofingcost.com.</p>
+                                            
+                                            <p>You recently put your information into our website to get a roofing estimate.</p>
+                                            
+                                            <p>Would you have a quick 5 minutes to discuss your project? I'm happy to give you advice or set you up with a locally licensed and vetted local, Idaho Contractor!</p>
+                                            
+                                            
+                                            <p>Let me know how I can help!</p>
+                                            
+                                             
+                                            <p><a id="btn" href="call: 121">Call Now</a></p>
+                                             
+                                            <p>Sincerely,</p>
+                                            <p>Jake</p>
 
-<p>You recently put your information into our website to get a roofing estimate.</p>
+                                        </td>
+                                    </tr>
+    
+    
+                                    <tr>
+                                        <td height="30"></td>
+                                    </tr>
+                                </tbody></table>
+                            </td>
+                        </tr>
+                    </tbody></table>
+                </td>
+            </tr>
+    
+    
+    <!-- END HEADER/BANNER -->
+    
+    
+    
+                            
+                        
+                    </tbody></table>
 
-<p>Would you have a quick 5 minutes to discuss your project? I'm happy to give you advice or set you up with a locally licensed and vetted local, Idaho Contractor!</p>
 
 
-<p>Let me know how I can help!</p>
-
- 
-<p><a id="btn" href="call: 121">Call Now Button</a></p>
- 
-<p>Sincerely,</p>
-<p>Jake</p>
 
 
-</div>
-</div>
-</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
 </body>
 </html>
